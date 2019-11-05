1bbe598eee8d322ca8e28b068d4e8a695ed6635b
k0rgana <anagrom1999@gmail.com>
2019-11-05 09:37:44 -0300

    renomeando migration

--- database/migrations/2019_11_05_025732_create_heroes_table.php
+++ database/migrations/2019_11_05_025732_create_heroes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableHeros extends Migration
+class CreateHeroesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTableHeros extends Migration
      */
     public function up()
     {
-        Schema::create('heros', function (Blueprint $table) {
+        Schema::create('heroes', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('alias');
@@ -29,6 +29,6 @@ class CreateTableHeros extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('heros');
+        Schema::dropIfExists('heroes');
     }
 }
